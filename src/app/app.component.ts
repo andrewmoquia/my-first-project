62d955961fad26eeae70262e171ed96a56c21b5c
andrewmoquia <andrewmoquia09@gmail.com>
2021-04-21 13:18:26 +0800

    Update App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Foods } from './foods'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'my-first-project';
+
+  firstName = "John";
+  secondName = "Andrew";
+  colors:string[] = ["Yellow", "Blue", "Orange"];
+
+  getFullName() {
+    return this.firstName + " " + this.secondName;
+  };
+
+  apple: Foods = {
+    type: 'Fruit',
+    name: 'Apple'
+  };
+
+  iceCream: Foods = {
+    type: 'Dessert',
+    name: 'Ice Cream'
+  };
+
+  menudo: Foods = {
+    type: 'Meal',
+    name: 'Menudo'
+  };
+
+  foods: Foods[ ] = [this.apple, this.iceCream, this.menudo];
 }
---
+++ src/app/foods.ts
@@ -0,0 +1,5 @@
+export interface Foods{
+    type: string,
+    name: string,
+    recipe?: string[]
+}
